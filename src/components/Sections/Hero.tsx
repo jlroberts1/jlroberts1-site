b2a1671c03203a1b51c9cc5c34a4202380c11bc7
James Roberts <105246977+jlroberts1@users.noreply.github.com>
2024-09-29 10:55:43 -0400

    Update main layout

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -21,42 +21,38 @@ const Hero: FC = memo(() => {
           src={imageSrc}
         />
         <div className="z-10  max-w-screen-lg px-4 lg:px-0">
-          <div className="relative">
-            <div className="flex flex-col items-center transform translate-y-1 z-0">
-              <Image
-                alt="Logo image"
-                className="rounded-full border-4 border-white shadow-lg"
-                height={175}
-                src={logoSrc}
-                width={175}
-              />
+          <Image
+            alt="Logo image"
+            className="rounded-full h-56 w-56 border-4 border-white shadow-lg mx-auto"
+            height={150}
+            src={logoSrc}
+            width={150}
+          />
+          <div className="flex flex-col items-center gap-y-6 rounded-xl bg-gray-800/40 p-6 text-center shadow-lg backdrop-blur-sm z-10 md:mt-[-20px]">
+            <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl">{name}</h1>
+            {description}
+            <div className="flex gap-x-4 text-neutral-100">
+              <Socials />
             </div>
-            <div className="flex flex-col items-center gap-y-6 rounded-xl bg-gray-800/40 p-6 text-center shadow-lg backdrop-blur-sm z-10 md:mt-[-20px]">
-              <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl">{name}</h1>
-              {description}
-              <div className="flex gap-x-4 text-neutral-100">
-                <Socials />
-              </div>
-              <div className="flex w-full justify-center gap-x-4">
-                {actions.map(({href, text, primary, Icon}) => (
-                  <a
-                    className={classNames(
-                      'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
-                      primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white',
-                    )}
-                    href={href}
-                    key={text}>
-                    {text}
-                    {Icon && <Icon className="h-5 w-5 text-white sm:h-6 sm:w-6" />}
-                  </a>
-                ))}
-              </div>
+            <div className="flex w-full justify-center gap-x-4">
+              {actions.map(({href, text, primary, Icon}) => (
+                <a
+                  className={classNames(
+                    'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
+                    primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white',
+                  )}
+                  href={href}
+                  key={text}>
+                  {text}
+                  {Icon && <Icon className="h-5 w-5 text-white sm:h-6 sm:w-6" />}
+                </a>
+              ))}
             </div>
           </div>
         </div>
         <div className="absolute inset-x-0 bottom-6 flex justify-center">
           <a
-            className="rounded-full bg-white p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
+            className="rounded-full bg-white p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2 animate-bounce"
             href={`/#${SectionId.About}`}>
             <ChevronDownIcon className="h-5 w-5 bg-transparent sm:h-6 sm:w-6" />
           </a>
