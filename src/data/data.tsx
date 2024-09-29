8c97dac5781baf2713361b6a63234c8dd93bb6dd
James Roberts <105246977+jlroberts1@users.noreply.github.com>
2024-09-28 21:12:28 -0400

    Fix resume loading

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -80,7 +80,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '../assets/resume.pdf',
+      href: 'assets/resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
